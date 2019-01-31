db36ed4c8fd5965e54df2809c5a47194e5941cc1
Charley McGowan and Sheila Stephen <charley+sheila@>
2019-01-31 08:37:26 -0800

    make the tshirt component working

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { AlbumService } from '../album.service';
+import { TshirtService } from '../tshirt.service'
 import { Album } from '../album.model';
+import { Tshirt } from '../tshirt.model';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css'],
-  providers: [AlbumService]
+  providers: [AlbumService, TshirtService]
 })
 export class AdminComponent implements OnInit {
 
-  constructor(private albumService: AlbumService) { }
+  constructor(private albumService: AlbumService, private tshirtService: TshirtService) { }
 
   ngOnInit() {
   }
@@ -18,6 +20,11 @@ export class AdminComponent implements OnInit {
   submitForm(title: string, artist: string, description: string){
     var newAlbum: Album = new Album(title, artist, description);
     this.albumService.addAlbum(newAlbum);
+  }
+
+  submitTshirtForm(brand: string, color: string, size: string, price: number){
+    var newTshirt: Tshirt = new Tshirt(brand, color, size, price);
+    this.tshirtService.addTshirt(newTshirt);
   }
 
 }
